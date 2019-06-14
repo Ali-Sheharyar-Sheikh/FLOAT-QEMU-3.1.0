5f58ee5d3f8c0278770a759e1cf7320b5fb0f59e
Ali Sheharyar Sheikh <xboxshery@gmail.com>
2019-06-14 16:32:02 +0500

    Added host side build and run notes

--- host-side/qemu-3.1.0/cudaforward.cu
+++ host-side/qemu-3.1.0/cudaforward.cu
@@ -43,7 +43,7 @@ extern "C" {
             char *deviceAddress,
             const char *deviceName,
             int ext,
-            int size,
+            size_t size,
             int constant,
             int global
             );
